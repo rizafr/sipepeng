ee9002fc3530b846edb65c1be09103f40166a3ec
rizafr <riza.fauzi.rahman@gmail.com>
2015-03-18 09:03:17 +0700

    edi menu_admin.php dan penambahan icon-icon menu

---
+++ application/views/admin/templates/menu_admin.php
@@ -0,0 +1,65 @@
+<aside>
+	<div id="sidebar"  class="nav-collapse ">
+		<!-- sidebar menu start-->
+		<ul class="sidebar-menu" id="nav-accordion">
+			<li>
+				<a href="<?php echo base_url(); ?>admin/homes">
+					<i class="icon-home"></i>
+					<span>Dashboard</span>
+				</a>
+			</li>
+
+			<li>
+				<a  href="<?php echo base_url() ?>admin/user_managements">
+					<i class="icon-user"></i>
+					<span>User Management</span>
+				</a>
+			</li>     	
+			
+			
+			<li>
+				<a  href="<?php echo base_url() ?>admin/user_managements">
+					<i class=" icon-bell-alt"></i>
+					<span>Data Awal</span>
+				</a>
+			</li>      
+			
+			<li>
+				<a  href="<?php echo base_url() ?>admin/user_managements">
+					<i class=" icon-thumbs-up"></i>
+					<span>Data Verifikasi</span>
+				</a>
+			</li> 
+			
+			<li>
+				<a  href="<?php echo base_url() ?>admin/user_managements">
+					<i class="icon-tasks"></i>
+					<span>Data Progres</span>
+				</a>
+			</li>      
+			
+			<li>
+				<a  href="<?php echo base_url() ?>admin/user_managements">
+					<i class="icon-check"></i>
+					<span>Data Selesai</span>
+				</a>
+			</li>  
+			
+			<li>
+				<a  href="<?php echo base_url() ?>admin/user_managements">
+					<i class="icon-check-minus"></i>
+					<span>Data Tidak Terverifikasi</span>
+				</a>
+			</li>   
+			
+			<li>
+				<a  href="<?php echo base_url() ?>admin/user_managements">
+					<i class="icon-print"></i>
+					<span>Laporan</span>
+				</a>
+			</li>                    
+			
+		</ul>
+		<!-- sidebar menu end-->
+	</div>
+</aside>
