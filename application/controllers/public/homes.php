11249a3c4e2b16a5906cd8b766ee6eee8ab21d70
rizafr <riza.fauzi.rahman@gmail.com>
2015-04-11 21:51:52 +0700

    memunculkan semua koordinat tapi baru drainase dan mck saja yg fix

--- application/controllers/public/homes.php
+++ application/controllers/public/homes.php
@@ -7,7 +7,6 @@ class Homes extends CI_Controller {
     function __construct() {
         parent::__construct();		
 		 $this->load->model('public_model');
-		 $this->load->model('drainase_model');
 		 # menampilkan google map
 		 $this->load->library('googlemaps');
     }
@@ -29,8 +28,21 @@ class Homes extends CI_Controller {
         $data['title'] = "Peta - SIPEPENG";
 		
 		//ambil koordinator drainase_list
-		$drainase_list = $this->drainase_model->select_all()->result();
-		//var_dump($drainase_list);
+		$drainase_list = $this->public_model->drainase_all()->result();
+		//ambil koordinator jalan_list
+		$jalan_list = $this->public_model->jalan_all()->result();
+		//ambil koordinator kirmir_list
+		$kirmir_list = $this->public_model->kirmir_all()->result();
+		//ambil koordinator mck_list
+		$mck_list = $this->public_model->mck_all()->result();
+		//ambil koordinator septictank_list
+		$septictank_list = $this->public_model->septictank_all()->result();
+		//ambil koordinator drainase_list
+		$septictank_komunal_list = $this->public_model->septictank_komunal_all()->result();
+		//ambil koordinator sumur_dangkal_list
+		$sumur_dangkal_list = $this->public_model->sumur_dangkal_all()->result();
+		//ambil koordinator drainase_list
+		$sumur_resapan_list = $this->public_model->sumur_resapan_all()->result();
 		
 		#google map yg bisa di klik otomatis dapetin koordinatnya
 		$config['center'] = '-6.900282, 107.530010';
@@ -62,6 +74,21 @@ class Homes extends CI_Controller {
 			$this->googlemaps->add_marker($marker);
 		}
 		#end tampilkan data koordinat drainase 
+               
+		#tampilkan data koordinat jalan
+		foreach ($mck_list as $row) {
+		
+			#ambil fotonya jika ada
+			$foto = isset($row->foto) ? "<img src='".base_url()."assets/upload/foto/".$row->foto."' height='150px' width='250px'/>" : 'Belum Ada foto';
+			
+			
+			$marker = array();
+			$marker['position'] = $row->lat.",".$row->long;
+			$marker['infowindow_content'] = "MCK <br />RW : ".$row->rw ." <br /> Alamat:  ". $row->alamat ."<br />".$foto ;
+			$marker['icon'] = 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=M|4999FF|000000';
+			$this->googlemaps->add_marker($marker);
+		}
+		#end tampilkan data koordinat jalan 
 		
 		
 		$data['map'] = $this->googlemaps->create_map();
--- application/models/public_model.php
+++ application/models/public_model.php
@@ -4,68 +4,122 @@ if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
 class Public_model extends CI_Model {
-    
-    
-    
+
     public function __construct() {
         parent::__construct();
     }
     
-	#jumlah user
-	public function getJumlahPengguna(){
-		return $this->db->count_all('pengguna');
-	}
-	
-	#jumlah artesis
-	public function getJumlahArtesis(){
-		return $this->db->count_all('artesis');
-	}
-	
-	#jumlah drainase
-	public function getJumlahDrainase(){
-		return $this->db->count_all('drainase');
-	}
-	
-	#jumlah jalan
-	public function getJumlahJalan(){
-		return $this->db->count_all('jalan');
-	}
-	#jumlah kirmir
-	public function getJumlahKirmir(){
-		return $this->db->count_all('kirmir');
-	}
-	
-	#jumlah mck
-	public function getJumlahMck(){
-		return $this->db->count_all('mck');
-	}
-	
-	#jumlah septictank
-	public function getJumlahSeptictank(){
-		return $this->db->count_all('septictank');
-	}
-	#jumlah septictank_komunal
-	public function getJumlahSeptictankKomunal(){
-		return $this->db->count_all('septictank_komunal');
-	}
-	
-	#jumlah sumur_dangkal
-	public function getJumlahSumurDangkal(){
-		return $this->db->count_all('sumur_dangkal');
-	}
-	
-	#jumlah sumur_resapan
-	public function getJumlahSumurResapan(){
-		return $this->db->count_all('sumur_resapan');
-	}
-	
-	
+    #ambil data artesis
+    public function artesis_all() {
+        $sql = "select * from artesis";
+        return $this->db->query($sql);
+    }
+    #ambil data drainase
+    public function drainase_all() {
+        $sql = "select * from drainase";
+        return $this->db->query($sql);
+    }
+    #ambil data jalan
+    public function jalan_all() {
+        $sql = "select * from jalan";
+        return $this->db->query($sql);
+    }
+    #ambil data kirmir
+    public function kirmir_all() {
+        $sql = "select * from kirmir";
+        return $this->db->query($sql);
+    }
+    #ambil data mck
+    public function mck_all() {
+        $sql = "select * from mck";
+        return $this->db->query($sql);
+    }
+    #ambil data septictank
+    public function septictank_all() {
+        $sql = "select * from septictank";
+        return $this->db->query($sql);
+    }
+    #ambil data septictank_komunal
+    public function septictank_komunal_all() {
+        $sql = "select * from septictank_komunal";
+        return $this->db->query($sql);
+    }
+    #ambil data sumur_dangkal
+    public function sumur_dangkal_all() {
+        $sql = "select * from sumur_dangkal";
+        return $this->db->query($sql);
+    }
+    #ambil data sumur_dangkal
+    public function sumur_resapan_all() {
+        $sql = "select * from sumur_resapan";
+        return $this->db->query($sql);
+    }
+
+    #jumlah user
+    public function getJumlahPengguna() {
+        return $this->db->count_all('pengguna');
+    }
+
+    #jumlah artesis
+
+    public function getJumlahArtesis() {
+        return $this->db->count_all('artesis');
+    }
+
+    #jumlah drainase
+
+    public function getJumlahDrainase() {
+        return $this->db->count_all('drainase');
+    }
+
+    #jumlah jalan
+
+    public function getJumlahJalan() {
+        return $this->db->count_all('jalan');
+    }
+
+    #jumlah kirmir
+
+    public function getJumlahKirmir() {
+        return $this->db->count_all('kirmir');
+    }
+
+    #jumlah mck
+
+    public function getJumlahMck() {
+        return $this->db->count_all('mck');
+    }
+
+    #jumlah septictank
+
+    public function getJumlahSeptictank() {
+        return $this->db->count_all('septictank');
+    }
+
+    #jumlah septictank_komunal
+
+    public function getJumlahSeptictankKomunal() {
+        return $this->db->count_all('septictank_komunal');
+    }
+
+    #jumlah sumur_dangkal
+
+    public function getJumlahSumurDangkal() {
+        return $this->db->count_all('sumur_dangkal');
+    }
+
+    #jumlah sumur_resapan
+
+    public function getJumlahSumurResapan() {
+        return $this->db->count_all('sumur_resapan');
+    }
+
     //select
-    public function select_all(){
+    public function select_all() {
         $sql = "select * from menu";
         return $this->db->query($sql);
     }
-    
+
     public function select_all_paging($limit = array()) {
         $sql = "select * from product_categories";
         if ($limit != null)
@@ -78,7 +132,7 @@ class Public_model extends CI_Model {
         $data = $this->db->query($sql);
         return $data;
     }
-    
+
     public function getCategoryById($id_category) {
         $this->db->select('*');
         $this->db->where('id_category', $id_category);
@@ -88,7 +142,7 @@ class Public_model extends CI_Model {
             return $query->row_array();
         }
     }
-    
+
     public function get_dropdown_list() {
         $this->db->from($this->table);
         $this->db->order_by('category_name', 'asc');
@@ -102,40 +156,7 @@ class Public_model extends CI_Model {
         }
         return $return;
     }
-    
-    public function add(){
-        $data = array(
-            'id_category' => ''
-            ,'category_name' => $this->input->post('category_name')
-            ,'deskripsi' => $this->input->post('deskripsi')
-            ,'created_at' => date('Y-m-d H:i:s', time()+60*60*6)
-        );
-        $this->db->insert($this->table, $data);
-        
-        if ($this->db->affected_rows() == 1) {
 
-            return TRUE;
-        }
-        return FALSE;
-    }
-    
-    //delete
-    public function delete($id_category) {
-        $sql = "delete from product_categories where id_category = " . $id_category;
-        $query = $this->db->query($sql);
-        return $query;
-    }
-    
-    //update
-    public function update($id_category){
-        $data = array(
-            'category_name' => $this->input->post('category_name')
-            ,'deskripsi' => $this->input->post('deskripsi')
-            ,'modified_at' => date('Y-m-d H:i:s', time()+60*60*6)
-        );
-        $this->db->where('id_category', $id_category);
-        return $this->db->update($this->table, $data);
-    }
 }
 
 ?>
