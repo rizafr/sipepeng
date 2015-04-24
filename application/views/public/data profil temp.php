4218196667de6280769bb5380e2f398fc75fa5e5
rizafr <riza.fauzi.rahman@gmail.com>
2015-04-24 16:40:17 +0700

    temporary data profil

---
+++ application/views/public/data profil temp.php
@@ -0,0 +1,260 @@
+<b>Profil Kelurahan Leuwigajah</b>
+
+Kelurahan Leuwigajah adalah salah satu wilayah kerja Lurah Leuwigajah di Kecamatan Cimahi Selatan Kota Cimahi dan secara geografis memiliki luas wilayah sebesar 393,473 Km<sup>2</sup>. Secara administratif mempunyai 20 RW dan 148 RT dengan jumlah penduduk sebesar 42.790 jiwa (<i>data bulan Mei 2014</i>). Wilayah Kelurahan Leuwigajah merupakan wilayah perdagangan dan jasa, permukiman, pusat pendidikan serta sebagian kecil merupakan wilayah perindustrian.
+
+&nbsp;
+
+<b>PETA KELURAHAN LEUWIGAJAH</b>
+
+<b> </b>
+
+<b>Visi Dan Misi </b>
+
+Dalam rangka pelaksanaan tugas pemerintahan dan pembangunan serta pelayanan masyarakat, Kelurahan Leuwigajah menetapkan visi yang mendukung terhadap Visi Kota Cimahi <i>“ Menuju Cimahi CERDAS ”</i> yaitu “<i>Menjaga dan mewujudkan masyarakat Leuwigajah yang taat </i><i>IBADAH</i><i>”</i> (Indah, Bersih, Agamis, Demokratis, Aman dan Harmonis) yang dijabarkan sebagai berikut :
+
+-      Indah               : yang berarti mewujudkan keindahan baik akhlak, mental maupun lingkungannya
+
+-      Bersih             : yang berarti budaya hidup bersih baik lingkungan maupun mental yang bersih jauh dari perbuatan yang tercela.
+
+-      Agamis           : yang berarti mewujudkan nilai-nilai keagamanan dalan kehidupan dimasyarakat
+
+-      Demokratis     : yang berarti mengandung pengertian bahwa dalam menyelesaikan permasalahan yang timbul di lingkungan masyarakat harus menjungjung tinggi nilai-nilai musyawarah untuk mufakat yang dilaksanakan secara demokratis
+
+-      Aman              : yang berarti menjaga lingkungan yang aman dan nyaman
+
+-      Harmonis        : yang berarti mewujudkan masyarakat yang dapat hidup berdampingan atau harmonis.
+
+Dalam upaya perwujudan Visi Kelurahan Leuwigajah Tahun 2012 – 2017 tersebut akan dicapai melalui 5 (lima) misi sebagai berikut :
+<ol>
+	<li>Meningkatkan kualitas pelayanan kepada masyarakat</li>
+</ol>
+Yaitu peningkatan pelayanan yang lebih efektif dan efisien guna mempercepat pelayanan pemerintahan kepada masyarakat.
+<ol>
+	<li>Meningkatkan pemberdayaan masyarakat dalam pembangunan</li>
+</ol>
+Yaitu mendorong terciptanya kemandirian masyarakat dalam ikut serta berpartisipasi dalam pembangunan diwilayahnya supaya lebih mandiri dan berkesinambungan.
+<ol>
+	<li>Meningkatkan peran sumber daya aparatur dan masyarakat dalam  pembangunan</li>
+</ol>
+Yaitu meningkatkan kapasitas aparatur dan peran serta masyarakat dalam menumbuh kembangkan inovasi kreatif dalam pembangunan diwilayahnya.
+<ol>
+	<li>Meningkatkan situasi dan kondisi masyarakat yang kondusif.</li>
+</ol>
+Yaitu mengoptimalkan pengamanan keamanan di lingkungannya secara bersama - sama sehingga mendukung lancarnya pembangunan.
+<ol>
+	<li>Meningkatkan kualitas sumber daya aparatur</li>
+</ol>
+Yaitu dengan meningkatkan kualitas aparatur yang profesional sehingga dapat mewujudkan kualitas pelayanan yang semakin baik.
+
+&nbsp;
+
+<b><br clear="all" /> </b>
+
+<b> </b>
+
+<b>Program dan Kegiatan</b>
+
+Prioritas dan program kegiatan disesuaikan dengan prioritas Pembangunan Daerah Kota Cimahi dan tingkat permasalahan serta tuntutan masyarakat yang disampaikan pada Musyawarah Perencanaan Pembangunan (Musrenbang) di tingkat kelurahan. Dengan demikian Kelurahan Leuwigajah pada Tahun 2015 menyusun urutan prioritas program dan kegiatan pembangunan berjumlah  11 (sebelas) program dan 32 (tiga dua) kegiatan, seperti tercantum  berikut ini :
+
+Urusan Wajib
+<ol>
+	<li>PERENCANAAN PEMBANGUNAN
+<ol>
+	<li>Program Perencanaan Pembangunan Daerah</li>
+	<li>Kegiatan Pelaksanaan Musyawarah Pembangunan Desa</li>
+</ol>
+</li>
+	<li>PROGRAM PENINGKATAN SISTEM PENGAWASAN INTERNAL DAN PENGENDALIAN PELAKSANAAN KEBIJAKAN KDH
+<ol>
+	<li>Kegiatan Pengendalian Manajemen Pelaksanaan Kebijakan KDH</li>
+</ol>
+</li>
+	<li>PEMBERDAYAAN MASYARAKAT DAN DESA
+<ol>
+	<li>Program Peningkatan Keberdayaan Masyarakat</li>
+</ol>
+</li>
+</ol>
+a.  Kegiatan Pemberdayaan Lembaga dan Organisasi Masyarakat Perdesaan
+
+b. Peningkatan Pelaksanaan Pembangunan Daerah
+<ol>
+	<li>KESATUAN BANGSA DAN POLITIK DALAM NEGERI
+<ol>
+	<li>Program Pengembangan Wawasan Kebangsaan, dengan Kegiatan Pelaksanaan PHBA dan PHBN.</li>
+	<li>Program Peningkatan Keamanan dan Kenyamanan Lingkungan, dengan Kegiatan Pembinaan Linmas</li>
+</ol>
+</li>
+	<li>OTDA, PEMERINTAHAN UMUM, ADM. KEUANGAN DAERAH, PERANGKAT DAERAH, KEPEGAWAIAN, DAN PERSANDIAN
+<ol>
+	<li>Program Pelayanan Administrasi Perkantoran, dengan kegiatan sebagai berikut :
+<ol>
+	<li>Penyediaan Jasa Surat Menyurat</li>
+	<li>Penyediaan Jasa Komunikasi, Sumber Daya Air dan Listrik</li>
+	<li>Penyediaan Jasa Jaminan Barang Milik Daerah</li>
+	<li>Penyediaan Jasa Pemeliharaan dan Perizinan Kendaraan Dinas/Operasional.</li>
+	<li>Penyediaan Jasa Administrasi Keuangan</li>
+	<li>Penyediaan Jasa Kebersihan Kantor</li>
+	<li>Penyediaan Jasa Perbaikan Peralatan Kerja</li>
+	<li>Penyediaan Alat Tulis Kantor</li>
+	<li>Penyediaan Barang Cetakan dan Penggandaan</li>
+	<li>Penyediaan Komponen Instlasi Listrik/Penerangan Bangunan Kantor</li>
+	<li>Penyediaan Bahan Bacaan dan Peraturan Perundang-undangan</li>
+	<li>Penyediaan Makanan Minuman</li>
+	<li>Rapat-rapat Koordinasi dan Konsultasi ke Luar Daerah</li>
+	<li>Penyediaan Jasa Pengamanan Kantor dan Rumah Dinas</li>
+	<li>Program Peningkatan Sarana dan Prasarana Aparatur, dengan kegiatan sebagai berikut :
+<ol>
+	<li>Pengadaan Meubeleur</li>
+	<li>Peningkatan Sarana dan Prasarana Aparatur</li>
+	<li>Pemeliharaan Rutin/Berkala Gedung Kantor</li>
+	<li>Pemeliharaan Rutin/Berkala Kendaraan Dinas/Operasional</li>
+	<li>Program Peningkatan Pengembangan Sistem Pelaporan Capaian Kinerja dan Keuangan, dengan kegiatan sebagai berikut :
+<ol>
+	<li>Penyusunan Laporan Capaian Kinerja dan Ikhtisar Realisasi Kinerja SKPD</li>
+	<li>Penyusunan Laporan Keuangan Semesteran</li>
+	<li>Penyusunan Pelaporan Keuangan Akhir Tahun</li>
+	<li>Program Peningkatan Kapasitas Sumberdaya Aparatur, dengan  kegiatan Pembinaan Pegawai</li>
+	<li>Program Peningkatan Disiplin Aparatur, dengan kegiatan sebagai berikut :
+<ol>
+	<li>Pengadaan Pakaian Dinas beserta Perlengkapannya</li>
+	<li>Pengadaan Pakaian KORPRI</li>
+	<li>Pengadaan Pakaian Khusus Hari-hari Tertentu</li>
+</ol>
+</li>
+</ol>
+</li>
+</ol>
+</li>
+</ol>
+</li>
+</ol>
+</li>
+</ol>
+&nbsp;
+
+Kewenangan Walikota yang dilimpahkan kepada lurah mencakup beberapa Bidang Kewenangan sesuai dengan Keputusan Walikota Cimahi Nomor 782/Kep 58 a-Pem/2004 Tanggal 19 April 2004 adalah sebagai berikut :
+<ol>
+	<li><b>I.        </b><b>Bidang Pekerjaan Umum :</b>
+<ol>
+	<li>Pendataan titik jalan penerangan jalan umum dan penataan serta  pemasangan lampu-lampu di lingkungan;</li>
+	<li>Memfasilitasi pengawasan pembangunan sarana Pemerintah;</li>
+	<li>Melaporkan pemanfaatan bangunan;</li>
+	<li>Pendataan bangunan;</li>
+	<li>Pendataan pengendalian bantaran sungai;</li>
+	<li>Pembangunan dan pemeliharaan taman lingkungan dan jalur hijau lingkungan;</li>
+	<li>Pendataan, pengawasan dan pengendalan tanah milik Pemerintah Kota;</li>
+	<li><b>II.      </b><b>Bidang</b> <b>Kesehatan :</b>
+<ol>
+	<li>Pengawasan Pelayanan Kesehatan Masyarakat;</li>
+	<li>Pembinaan Kegiatan GSI (Gerakan Sayang Ibu), SKPG (Sistim Kewaspadaan Pangan dan Gizi), Komite Penanggulangan AIDS, Gerdunas (Gerakan Terpadu Nasional TBC), JPS-BK (Jaringan Pengaman Sosial Bidang Kesehatan);</li>
+	<li>Penanganan/Koordinator Kejadian (KLB) yang bersumber penyakit.</li>
+	<li><b>III.    </b><b>Bidang Penanaman Modal :</b>
+<ol>
+	<li>Pendataan dan pengawasan Usaha Kecil dan Menengah (UKM) dalam rangka promosi produk unggulan/andalan.</li>
+	<li><b>IV.            </b><b>Bidang Lingkungan Hidup :</b>
+<ol>
+	<li>Pengkoordinasian penyediaan sarana dan prasarana kebersihan lingkungan masyarakat;</li>
+	<li>Inventarisasi mengenai pohon-pohon lahan kritis, pelindung dan hias;</li>
+	<li>Penertiban pembuangan sampah di sungai dan saluran;</li>
+	<li>Pemasyarakatan pembuatan sumur resapan;</li>
+	<li>Pengawasan dan penyampaian informasi mengenai Taman dan PJU;</li>
+	<li>Melaksanakan Sosialisasi mengenai pengelolaan persampahan pada masyarakat;</li>
+	<li>Pengawasan limbah industri;</li>
+</ol>
+</li>
+</ol>
+</li>
+</ol>
+</li>
+</ol>
+</li>
+	<li><b>V.      </b><b>Bidang Pertanahan :</b>
+<ol>
+	<li>Pelaporan, pengawasan dan pengendalian tanah milik/asset pemerintah;</li>
+	<li>Monitoring dan pelaporan peralihan hak atas tanah dan bangunan maupun peruntukannya.</li>
+</ol>
+</li>
+	<li><b>VI.    </b><b>Bidang Koperasi dan Usaha Kecil Menengah :</b>
+<ol>
+	<li>Pelaksanaan Monitoring kegiatan Koperasi dan Pengusaha Kecil Menengah (PKM).</li>
+</ol>
+</li>
+	<li><b>VII.  </b><b>Bidang Kependudukan dan Catatan Sipil :</b>
+<ol>
+	<li>Pelayanan pembuatan KK (Kartu Keluarga), KTP (Kartu Tanda Penduduk) dan Kartu NIK, Surat Keterangan Lahir dan Surat Keterangan Susunan Ahli Waris;</li>
+	<li>Penandatangan dan penerbitan Surat Keterangan Kematian, Surat Keterangan Pindah antar Kelurahan dan Kecamatan;</li>
+	<li>Pelayanan penerbitan Surat Keterangan Perkawinan:</li>
+	<li>Pelayanan penerbitan Surat Bukti Pendaftaran Tamu;</li>
+	<li>Pendataan identitas penduduk musiman;</li>
+	<li>Pelayanan penerbitan Surat Keterangan jaminan bertempat tinggal;</li>
+	<li>Pelayanan penerbitan Surat Keterangan berpenghasilan bagi wiraswasta;</li>
+	<li>Pelayanan penerbitan Surat Pengantar pindah antar daerah kota/kabupaten;</li>
+	<li>Pelaksanaan penyuluhan kependudukan dan catatan sipil;</li>
+	<li>Monitoring dan evaluasi kependudukan;</li>
+</ol>
+</li>
+	<li><b>VIII.        </b><b>Bidang Pengembangan Otonomi Daerah :</b>
+<ol>
+	<li>Pelaksanaan pembinaan penyelenggaraan Pemerintahan Kelurahan;</li>
+	<li>Pembinaan pendataan monografi Kelurahan;</li>
+	<li>Pembinaan pengisian buku administrasi Pemerintahan Kelurahan;</li>
+	<li>Fasilitas pemberhentian, pemilihan, pelantikan dan pengesahan Ketua RT/RW;</li>
+	<li>Membina kehidupan masyarakat dan penanganan masalah kemasyarakatan</li>
+</ol>
+</li>
+	<li><b>IX.    </b><b>Bidang Pendapatan Daerah :</b>
+<ol>
+	<li>Monitoring pembayaran PBB (Pajak Bumi Bangunan) dari Masyarakat ke Bank;</li>
+	<li>Inventarisasi NJOP (NIlai Jual Objek Pajak);</li>
+	<li><b>X.              </b><b>Bidang Hukum dan Perundang-undangan :</b>
+<ol>
+	<li>Fasilitasi sosialisasi peraturan perundang-undangan dari produk hukum daerah;</li>
+	<li>Fasilitasi monitoring pelaksanaan dan penegakan peraturan daerah.</li>
+</ol>
+</li>
+</ol>
+</li>
+	<li><b>XI.    </b><b>Bidang Politik Dalam Negeri dan Administrasi Publik :</b>
+<ol>
+	<li>Membantu penyelenggaraan Pemilu;</li>
+	<li>Pembinaan ketentraman dan ketertiban sesuai dengan kebijaksanaan yang telah ditetapkan;</li>
+	<li>Penerbitan rekomendasi atas permohonan ijin keamanan;</li>
+	<li>Penerbitan rekomendasi SKKB (Surat Keterangan Kelakuan Baik).</li>
+</ol>
+</li>
+	<li><b>XII.  </b><b>Bidang Sosial :</b>
+<ol>
+	<li>Pelaksanaan usaha-usaha preventif dan respresif apabila terjadi atau diperkirakan terjadi bencana alam;</li>
+	<li>Penyaluran bantuan bagi korban bencana alam;</li>
+	<li>Pendataan Keluarga Pra KS;</li>
+</ol>
+</li>
+	<li><b>XIII.        </b><b>Bidang Pendidikan dan Kebudayaan :</b>
+<ol>
+	<li>Pendataan dan pembinaan pendidikan luar sekolah;</li>
+	<li>Pendataan dan pembinaan sarana dan prasarana pendidikan dasar, pemuda dan wanita.</li>
+</ol>
+</li>
+</ol>
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+<b>Struktur Organisasi Dan Pejabat Struktural </b><b>Kelurahan Leuwigajah</b><b></b>
+
+Berdasarkan Peraturan Daerah Kota Cimahi Nomor 5 Tahun 2012 tentang Kecamatan dan Kelurahan di Kota Cimahi, bahwa susunan organisasi kelurahan terdiri dari :
+<ol>
+	<li>Lurah</li>
+	<li>Sekretaris</li>
+	<li>Seksi Pemerintahan</li>
+	<li>Seksi Ekonomi dan Pembangunan</li>
+	<li>Seksi Pemberdayaan Masyarakat dan Kesejahteraan Rakyat</li>
+	<li>Seksi Ketentraman dan Ketertiban Umum</li>
+	<li>Kelompok Jabatan Fungsional</li>
+</ol>
+&nbsp;
+
+<b>DIAGRAM ORGANISASI DI LAMPIRAN STRUKTUR ORGANISASI.XLS</b>
