fc50fb32d620c1286019fef2d83188ff3121facc
ratihpujihati <ratih.pujihati@gmail.com>
2015-05-05 10:42:08 +0700

    update umkm list

--- application/views/admin/umkm/umkm_list.php
+++ application/views/admin/umkm/umkm_list.php
@@ -37,10 +37,7 @@
                                                 <tr>
                                                     <th>No</th>
 													<th>RT</th>
-													<th>RW</th>
-													<th>Kelurahan</th>
-													<th>Kecamatan</th>
-													<th>Kota</th>
+													<th>RW</th>													
 													<th>NBS</th>
 													<th>Klasifikasi Perusahaan</th>
 													<th>Nama Perusahaan</th>
@@ -48,19 +45,9 @@
 													<th>Kegiatan Utama</th>
 													<th>Jenis Usaha</th>
 													<th>JK Pengusaha</th>
-													<th>Kota Tempat Lahir Pengusaha</th>
-													<th>Prov Tempat Lahir Pengusaha</th>
 													<th>Bentuk Badan Hukum</th>
 													<th>Tahun Mulai Operasi</th>
 													<th>Gabung Paguyuban</th>
-													<th>Bermitra</th>
-													<th>Bentuk Kemitraan</th>
-													<th>Pembinaan Pemerintah</th>
-													<th>Pemberi Pembinaan</th>
-													<th>Eksport</th>
-													<th>Persentase Eksport</th>
-													<th>Import</th>
-													<th>Persentase Import</th>
                                                     <th>Aksi</th>
                                                 </tr>
                                             </thead>
@@ -76,9 +63,6 @@
                                                             <td><?php echo $no++ ?></td>
                                                             <td><?php echo $row->rt ?></td> 
                                                             <td><?php echo $row->rw ?></td> 
-															<td><?php echo $row->kelurahan ?></td>
-															<td><?php echo $row->kecamatan ?></td>
-															<td><?php echo $row->kota ?></td>
 															<td><?php echo $row->nbs ?></td>
 															<td><?php echo $row->klasifikasi_perusahaan ?></td>
 															<td><?php echo $row->nama_perusahaan ?></td>
@@ -86,19 +70,9 @@
 															<td><?php echo $row->kegiatan_utama ?></td>
 															<td><?php echo $row->jenis_usaha ?></td>
 															<td><?php echo $row->jk_pengusaha ?></td>
-															<td><?php echo $row->kota_tempat_lahir_pengusaha ?></td>
-															<td><?php echo $row->prov_tempat_lahir_pengusaha ?></td>
 															<td><?php echo $row->bentuk_badan_hukum ?></td>									                                                            
 															<td><?php echo $row->tahun_mulai_operasi ?></td>									                                                            
-															<td><?php echo $row->gabung_paguyuban ?></td>									                                                            
-															<td><?php echo $row->bermitra ?></td>									                                                            
-															<td><?php echo $row->bentuk_kemitraan ?></td>									                                                            
-															<td><?php echo $row->pembinaan_pemerintah ?></td>									                                                            
-															<td><?php echo $row->pemberi_pembinaan ?></td>									                                                            
-															<td><?php echo $row->eksport ?></td>                                                             
-															<td><?php echo $row->persentase_eksport ?></td>                                                             
-															<td><?php echo $row->import ?></td>                                                             
-															<td><?php echo $row->persentase_import ?></td> 
+															<td><?php echo $row->gabung_paguyuban ?></td>	
 															<td>
                                                                 <a href="<?php echo base_url() . 'admin/umkm_managements/gotoForm/edit/' . $row->id_umkm ?>"><button class = "btn btn-primary" title="Edit"><i class = "icon-pencil"></i></button></a>
                                                                 <a href="<?php echo base_url() . 'admin/umkm_managements/delete/' . $row->id_umkm ?>" onclick="return confirm('Are you sure want to delete this?');"><button class = "btn btn-danger" title="Delete"><i class = "icon-trash "></i></button></a>
