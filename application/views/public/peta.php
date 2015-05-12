1ec5bfc2f1ec7c33fb8dbc6f567e0af4557d87d7
rizafr <riza.fauzi.rahman@gmail.com>
2015-05-12 11:49:25 +0700

    merapikan peta

--- application/views/public/peta.php
+++ application/views/public/peta.php
@@ -4,17 +4,17 @@
 <body class="homepage">
     <?php $this->load->view('public/templates/menu'); ?> 
     <section id="peraturan" >
-        <div class="container>
-             <div class="row">
-             <?php echo $map['html']; ?>
-    </div>
-    </div>
-</section>
+        <div class="container">
+            <div class="row">
+                <?php echo $map['html']; ?>
+            </div>
+        </div>
+    </section>
 
 
-<script type="text/javascript">
-    function getLokasi(newLat, newLng)
-    {
-        alert(newLat);
-    }
-</script>
+    <script type="text/javascript">
+        function getLokasi(newLat, newLng)
+        {
+            alert(newLat);
+        }
+    </script>
