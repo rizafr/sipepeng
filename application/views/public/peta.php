7dc0a61231a79fbce4cd83bd6862b9537e6312df
rizafr <riza.fauzi.rahman@gmail.com>
2015-05-12 12:43:23 +0700

    merapikan peta.php

--- application/views/public/peta.php
+++ application/views/public/peta.php
@@ -3,18 +3,72 @@
 
 <body class="homepage">
     <?php $this->load->view('public/templates/menu'); ?> 
-    <section id="peraturan" >
-        <div class="container">
-            <div class="row">
-                <?php echo $map['html']; ?>
-            </div>
+
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand">
+                    <a href="#">
+                    </a>
+                </li>
+                <li>
+                    <a href="#">Dashboard</a>
+                </li>
+                <li>
+                    <a href="#">Shortcuts</a>
+                </li>
+                <li>
+                    <a href="#">Overview</a>
+                </li>
+                <li>
+                    <a href="#">Events</a>
+                </li>
+                <li>
+                    <a href="#">About</a>
+                </li>
+                <li>
+                    <a href="#">Services</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
         </div>
-    </section>
+        <!-- /#sidebar-wrapper -->
 
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+		<section id="peraturan">
+            <div class="container-fluid">
+			<div class="get-started center wow fadeInDown">
+                <h2>PETA PEMBANGUNAN		 <BR />										
+					KELURAHAN LEUWIGAJAH							
+				</h2>
+			</div>
+                <div class="row wow fadeInDown"">
+                    <div class="col-lg-12">
+                        <?php echo $map['html']; ?>
+                        <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Tampilkan Pilihan Kegiatan</a>
+                    </div>
+                </div>
+            </div>
+			</section>
+        </div>
+        <!-- /#page-content-wrapper -->
 
-    <script type="text/javascript">
-        function getLokasi(newLat, newLng)
-        {
-            alert(newLat);
-        }
-    </script>
+    </div>
+    <!-- /#wrapper -->
+    
+<script src="<?php echo base_url() ?>assets/public/js/jquery.js"></script>
+<script type="text/javascript" charset="utf-8">
+    $(document).ready(function() {
+		<!-- Menu Toggle Script -->
+		$("#menu-toggle").click(function(e) {
+			e.preventDefault();
+			$("#wrapper").toggleClass("toggled");
+		});
+	} );
+	
+</script>
--- application/views/public/templates/footer.php
+++ application/views/public/templates/footer.php
@@ -1,43 +1,50 @@
 <footer id="footer" class="midnight-blue">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-12">
-                    <p align="center">SIPEPENG | Sistem Informasi Pemetaan Pembangunan <br />
-						Proyek Perubahan Diklatpim IV - DEVI JANUAR HADI, S.Si, M.Si  <br /> 
-						<small><i>Developed by Ratih Pujihati dan  Riza Fauzi Rahman</i> </small> </p>
-                </div>                
-            </div>
-        </div>
-    </footer><!--/#footer-->
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<p align="center">SIPEPENG | Sistem Informasi Pemetaan Pembangunan <br />
+					Proyek Perubahan Diklatpim IV - DEVI JANUAR HADI, S.Si, M.Si  <br /> 
+				<small><i>Developed by Ratih Pujihati dan  Riza Fauzi Rahman</i> </small> </p>
+			</div>                
+		</div>
+	</div>
+</footer><!--/#footer-->
 
-    <script src="<?php echo base_url() ?>assets/public/js/jquery.js"></script>
-    <script src="<?php echo base_url() ?>assets/public/js/bootstrap.min.js"></script>
-    <script src="<?php echo base_url() ?>assets/public/js/jquery.prettyPhoto.js"></script>
-    <script src="<?php echo base_url() ?>assets/public/js/jquery.isotope.min.js"></script>
-    <script src="<?php echo base_url() ?>assets/public/js/main.js"></script>
-    <script src="<?php echo base_url() ?>assets/public/js/wow.min.js"></script>
-	
-	<script src="<?php echo base_url(); ?>assets/admin/js/jquery.js"></script>
+<script src="<?php echo base_url() ?>assets/public/js/jquery.js"></script>
+<script src="<?php echo base_url() ?>assets/public/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url() ?>assets/public/js/jquery.prettyPhoto.js"></script>
+<script src="<?php echo base_url() ?>assets/public/js/jquery.isotope.min.js"></script>
+<script src="<?php echo base_url() ?>assets/public/js/main.js"></script>
+<script src="<?php echo base_url() ?>assets/public/js/wow.min.js"></script>
+
+<script src="<?php echo base_url(); ?>assets/admin/js/jquery.js"></script>
 <script src="<?php echo base_url(); ?>assets/admin/js/jquery-1.8.3.min.js"></script>
-	<script type="text/javascript" src="<?php echo base_url(); ?>assets/admin/assets/data-tables/jquery.dataTables.js"></script>
-	<script type="text/javascript" language="javascript" src="<?php echo base_url(); ?>assets/admin/assets/advanced-datatable/media/js/jquery.dataTables.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/admin/assets/data-tables/jquery.dataTables.js"></script>
+<script type="text/javascript" language="javascript" src="<?php echo base_url(); ?>assets/admin/assets/advanced-datatable/media/js/jquery.dataTables.js"></script>
 </body>
 </html>
- <script>
-$(window).scroll(function() {
-  if ($(document).scrollTop() > 75) {
-    $('nav').addClass('small-height');
-  } else {
-    $('nav').removeClass('small-height');
-  }
-});
-
+<script>
+	$(window).scroll(function() {
+		if ($(document).scrollTop() > 75) {
+			$('nav').addClass('small-height');
+			} else {
+			$('nav').removeClass('small-height');
+		}
+	});
+	
 </script>
 
 <script type="text/javascript" charset="utf-8">
     $(document).ready(function() {
         $('#example').dataTable( {
             "aaSorting": [[ 0, "asc" ]]
-        } );
-    } );
+		} );
+		<!-- Menu Toggle Script -->
+		$("#menu-toggle").click(function(e) {
+			e.preventDefault();
+			$("#wrapper").toggleClass("toggled");
+		});
+	} );
+	
 </script>
+
